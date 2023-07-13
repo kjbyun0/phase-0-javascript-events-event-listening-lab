3e8d55103d796e74b8df79a0f4cfc22ee07c2730
Ki Jong Byun <kjbyun0@gmail.com>
2023-07-13 07:48:07 -0500

    Assignment Done Twice

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addingEventListener() {
-    const btn = document.getElementById('button');
-    btn.addEventListener('click', function() {alert("I was clicked!");});
+    const btnElem = document.getElementById('button');
+    btnElem.addEventListener('click', () => alert('I was clicked'));
 }
 
-addingEventListener();
+// addingEventListener();
